ceec5b9e10bee99cc226b590948c395a11ca7766
sawangpong muadphet <sawangpongm@gmail.com>
2012-05-06 12:58:29 +0700

    initial itbshopping

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Itbshopping::Application.config.secret_token = 'db7b61e4252cd21369b51844ed1bfb82db772d945656c4324f94e8d7246803b1f4bcdf00703585f937a4ac485f5697fadd726cc797e1be39cd330baf089b82f3'
